44fb2c29bd81e0aeb510ab12c649ec5ae1dee052
arlez <rainmake123@gmail.com>
2021-11-30 17:19:52 -0300

    proyecto finalizado

--- src/App.js
+++ src/App.js
@@ -1,24 +1,59 @@
-import logo from './logo.svg';
-import './App.css';
+import React, {Fragment, useState, useEffect} from "react";
+import Formulario from "./components/Formulario";
+import Cancion from "./components/Cancion";
+import axios from "axios";
+import Info from "./components/Info";
 
 function App() {
+
+  const [busquedaletra, guardarBusquedaLetra] = useState({});
+  const [letra, guardarLetra] = useState([]);
+  const [info, guardarInfo] = useState({}); 
+
+  useEffect( ()=> {
+    if(Object.keys(busquedaletra).length === 0)return;
+
+    const {artista, cancion} = busquedaletra;
+
+    const consultarApiLetra = async () => {
+      const url = `https://api.lyrics.ovh/v1/${artista}/${cancion}`;
+      const url2 = `https://theaudiodb.com/api/v1/json/2/search.php?s=${artista}`;
+
+      const [letra , informacion] = await Promise.all([
+        axios.get(url),
+        axios.get(url2)
+      ])
+
+      guardarLetra(letra.data.lyrics);
+      guardarInfo(informacion.data.artists[0]);
+      //guardarLetra(respuesta.lyrics);
+    }
+
+    consultarApiLetra();
+
+  }, [busquedaletra]);
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Fragment>
+      <Formulario
+        guardarBusquedaLetra={guardarBusquedaLetra}
+      />
+
+      <div className="container mt-5">
+        <div className="row">
+          <div className="col-md-6">
+            <Info
+              info={info}
+            />
+          </div>
+          <div className="col-md-6">
+            <Cancion
+              letra={letra}
+            />
+          </div>
+        </div>
+      </div>
+    </Fragment>
   );
 }
 
